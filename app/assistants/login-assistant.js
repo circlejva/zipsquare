94d61888679d4b1da326d235789674e46e362b66
David Strack <rmx.dave@gmail.com>
2011-03-19 18:34:30 -0500

    Code cleanup and removing unused files

--- app/assistants/login-assistant.js
+++ app/assistants/login-assistant.js
@@ -1,91 +1,82 @@
 function LoginAssistant() {}
 
-LoginAssistant.prototype.setup = function() {
-	$$('body')[0].addClassName('clouds');
-	this.controller.setupWidget("txtUser",
-        this.userAttributes = {
-            hintText: $L("Email address or phone"),
-			textCase: Mojo.Widget.steModeLowerCase,
-			autoFocus: true
-        },
-        this.userModel = {
-            value: ""
-        }
-    ); 
-	this.controller.setupWidget("txtPassword",
-        this.passwordAttributes = {
-			hintText: $L("Password")
-        },
-        this.passwordModel = {
-            value: ""
-        }
-    ); 
-	this.controller.setupWidget("loginButton",
-         this.lbAttributes = {
-		 	
-             },
-         this.lbModel = {
-             label : "Login",
-             disabled: false
-         }
-    );
-	this.controller.listen(this.controller.get("loginButton"), Mojo.Event.tap, this.doLogin.bind(this));
-}
-LoginAssistant.prototype.doLogin = function(event){
-	toggleLoading("on");
-	var user = this.userModel.value;
-	var password = this.passwordModel.value;
-	var hash = Base64.encode(user + ":" + password);		
-	var url = foursquare.apibase + 'user.json';
-	var request = new Ajax.Request(url, {
-		method: 'get',
-		evalJSON: 'force',
-		requestHeaders: {
-			Authorization: "Basic " + hash
-		},
-		onSuccess: this.gotUser.bind(this),
-		onFailure: function(transport){
-			var r = transport.responseJSON;
-			ex(r.unauthorized);
-		}
-	});
-}
-LoginAssistant.prototype.gotUser = function(transport){
-	var u = transport.responseJSON.user;
-	user.auth = true;
-	user.id = u.id;
-	user.name = u.firstname + " " + u.lastname;
-	user.photo = u.photo;
-	user.login = this.userModel.value;
-	user.password = this.passwordModel.value;
-	user.fb = u.settings.sendtofacebook;
-	user.twitter = u.settings.sendtotwitter;
-	this.cookie = new Mojo.Model.Cookie('zsqUser');
-	this.cookie.put({
-			auth: true,
-			name: user.name,
-			photo: user.photo,
-			login: user.login,
-			password: user.password,
-			id: user.id,
-			fb: user.fb,
-			twitter: user.twitter
-	});	
-	Mojo.Controller.stageController.swapScene({
-		'name': 'venues',
-		transition: Mojo.Transition.crossFade
-	}); 
-}
-LoginAssistant.prototype.activate = function(event) {
+LoginAssistant.prototype = {
+	setup: function() {
+		$$('body')[0].addClassName('clouds');
+		this.controller.setupWidget("txtUser",
+	        this.userAttributes = {
+	            hintText: $L("Email address or phone"),
+				textCase: Mojo.Widget.steModeLowerCase,
+				autoFocus: true
+	        },
+	        this.userModel = {
+	            value: ""
+	        }
+	    ); 
+		this.controller.setupWidget("txtPassword",
+	        this.passwordAttributes = {
+				hintText: $L("Password")
+	        },
+	        this.passwordModel = {
+	            value: ""
+	        }
+	    ); 
+		this.controller.setupWidget("loginButton",
+	         this.lbAttributes = {
 
-}
-
-LoginAssistant.prototype.deactivate = function(event) {
-	/* remove any event handlers you added in activate and do any other cleanup that should happen before
-	   this scene is popped or another scene is pushed on top */
-}
-
-LoginAssistant.prototype.cleanup = function(event) {
-	/* this function should do any cleanup needed before the scene is destroyed as 
-	   a result of being popped off the scene stack */
-}
+	             },
+	         this.lbModel = {
+	             label : "Login",
+	             disabled: false
+	         }
+	    );
+		this.controller.listen(this.controller.get("loginButton"), Mojo.Event.tap, this.doLogin.bind(this));
+	},
+	
+	doLogin: function(event){
+		toggleLoading("on");
+		var user = this.userModel.value;
+		var password = this.passwordModel.value;
+		var hash = Base64.encode(user + ":" + password);		
+		var url = foursquare.apibase + 'user.json';
+		var request = new Ajax.Request(url, {
+			method: 'get',
+			evalJSON: 'force',
+			requestHeaders: {
+				Authorization: "Basic " + hash
+			},
+			onSuccess: this.gotUser.bind(this),
+			onFailure: function(transport){
+				var r = transport.responseJSON;
+				ex(r.unauthorized);
+			}
+		});
+	},
+	
+	gotUser: function(transport){
+		var u = transport.responseJSON.user;
+		user.auth = true;
+		user.id = u.id;
+		user.name = u.firstname + " " + u.lastname;
+		user.photo = u.photo;
+		user.login = this.userModel.value;
+		user.password = this.passwordModel.value;
+		user.fb = u.settings.sendtofacebook;
+		user.twitter = u.settings.sendtotwitter;
+		this.cookie = new Mojo.Model.Cookie('zsqUser');
+		this.cookie.put({
+				auth: true,
+				name: user.name,
+				photo: user.photo,
+				login: user.login,
+				password: user.password,
+				id: user.id,
+				fb: user.fb,
+				twitter: user.twitter
+		});	
+		Mojo.Controller.stageController.swapScene({
+			'name': 'venues',
+			transition: Mojo.Transition.crossFade
+		}); 
+	}
+};
--- app/assistants/new-assistant.js
+++ app/assistants/new-assistant.js
@@ -3,124 +3,108 @@ function NewAssistant(lat, lng) {
 	this.lng = lng;
 }
 
-NewAssistant.prototype.setup = function() {
-	this.resultsModel = {items: []};   
-	this.controller.setupWidget('results-list', 
-	      {itemTemplate:'templates/google-item', listTemplate:'templates/results-list'},
-	      this.resultsModel);
-    this.controller.setupWidget("txtSearch",
-        this.searchAttributes = {
-            hintText: $L("Venue name..."),
-            multiline: false,
-            enterSubmits: false,
-            focus: true
-         },
-         this.searchModel = {
-             value: "",
-             disabled: false
-         }
-    ); 
-    this.controller.setupWidget("btnSearch",
-         this.attributes = {
-             },
-         this.model = {
-             label : "Search Google",
-             disabled: false
-         }
-     );	
-	 this.controller.listen("btnSearch", Mojo.Event.tap, this.btnSearchTapped.bind(this));
-	 this.controller.listen("results-list", Mojo.Event.listTap, this.listTapped.bind(this));
-};
-NewAssistant.prototype.btnSearchTapped = function(event){
-	if (this.searchModel.value.length > 0){
-		this.searchGoogle();	
-	}
-};
-NewAssistant.prototype.searchGoogle = function(){
-	toggleLoading("on");
-	var coords = this.lat + "," + this.lng;
-	var url = 'http://ajax.googleapis.com/ajax/services/search/local?v=1.0&q='+ encodeURIComponent(this.searchModel.value) +'&rsz=small&near=' + encodeURIComponent(coords);
-	var request = new Ajax.Request(url, {
-		method: 'get',
-		evalJSON: 'force',
-		onSuccess: this.gotResults.bind(this),
-		onFailure: function(){
-			ex("Error getting Google results");
+NewAssistant.prototype = {
+	setup: function() {
+		this.resultsModel = {items: []};   
+		this.controller.setupWidget('results-list', 
+		      {itemTemplate:'templates/google-item', listTemplate:'templates/results-list'},
+		      this.resultsModel);
+	    this.controller.setupWidget("txtSearch",
+	        this.searchAttributes = {
+	            hintText: $L("Venue name..."),
+	            multiline: false,
+	            enterSubmits: false,
+	            focus: true
+	         },
+	         this.searchModel = {
+	             value: "",
+	             disabled: false
+	         }
+	    ); 
+	    this.controller.setupWidget("btnSearch",
+	         this.attributes = {
+	             },
+	         this.model = {
+	             label : "Search Google",
+	             disabled: false
+	         }
+	     );	
+	},
+	btnSearchTapped: function(event){
+		if (this.searchModel.value.length > 0){
+			this.searchGoogle();	
 		}
-	});	
-};
-NewAssistant.prototype.gotResults = function(transport){
-	toggleLoading("off");
-	var r = transport.responseJSON.responseData;
-	this.resultsModel.items = r.results;
-	this.controller.modelChanged(this.resultsModel);
-};
-NewAssistant.prototype.listTapped = function(event){
-	var m = "Add " + event.item.titleNoFormatting + " to Foursquare?";
-	this.controller.showAlertDialog({
-	    onChoose: function(value) {
-			if(value == 'ok'){
-				this.addVenue(event.item);
-			}else if(value =='cancel'){
-				//close the dialog
+	},
+	searchGoogle: function(){
+		toggleLoading("on");
+		var coords = this.lat + "," + this.lng;
+		var url = 'http://ajax.googleapis.com/ajax/services/search/local?v=1.0&q='+ encodeURIComponent(this.searchModel.value) +'&rsz=small&near=' + encodeURIComponent(coords);
+		var request = new Ajax.Request(url, {
+			method: 'get',
+			evalJSON: 'force',
+			onSuccess: this.gotResults.bind(this),
+			onFailure: function(){
+				ex("Error getting Google results");
 			}
-		},
-		title: $L("Add Venue?"),
-		message: m,
-		choices:[
-			{label: $L('Add Venue'), value:'ok', type:'affirmative'},
-			{label: $L('Cancel'), value:'cancel', type:'color'}    
-		]
-	});	 	
-};
-NewAssistant.prototype.addVenue = function(item){
-	banner("Adding " + item.titleNoFormatting + "...");
-	toggleLoading("on");
-	var hash = Base64.encode(user.login + ":" + user.password);
-	var params = "name=" + encodeURIComponent(item.titleNoFormatting) + "&address=" + encodeURIComponent(item.streetAddress) + "&city=" + encodeURIComponent(item.city) + "&state=" + encodeURIComponent(item.region) + "&geolat=" + encodeURIComponent(item.lat) + "&geolong=" + encodeURIComponent(item.lng);
-	var url = foursquare.apibase + 'addvenue.json';
-	var request = new Ajax.Request(url, {
-		method: 'POST',
-		evalJSON: 'force',
-		parameters: params,
-		requestHeaders: {
-				Authorization: "Basic " + hash
+		});	
+	},
+	gotResults: function(transport){
+		toggleLoading("off");
+		var r = transport.responseJSON.responseData;
+		this.resultsModel.items = r.results;
+		this.controller.modelChanged(this.resultsModel);
+	},
+	listTapped: function(event){
+		var m = "Add " + event.item.titleNoFormatting + " to Foursquare?";
+		this.controller.showAlertDialog({
+		    onChoose: function(value) {
+				if(value == 'ok'){
+					this.addVenue(event.item);
+				}else if(value =='cancel'){
+					//close the dialog
+				}
 			},
-		onSuccess: this.addedVenue.bind(this),
-		onFailure: function(transport){
-			toggleLoading("off");
-			ex(transport.responseJSON.error);
-		}
-	});	
-};
-NewAssistant.prototype.addedVenue = function(transport){
-	toggleLoading("off");
-	banner("Venue " + transport.responseJSON.venue.name + " added!");
-	Mojo.Controller.stageController.popScene();
-//	var v = transport.responseJSON.venue;
-//	var m = v.name + " successfully added! Check-in there?";
-//	this.controller.showAlertDialog({
-//	    onChoose: function(value) {
-//			if(value == 'ok'){
-//				this.addVenue(event.item);
-//			}else if(value =='cancel'){
-//				Mojo.Controller.stageController.popScene();
-//			}
-//		},
-//		title: $L("Success!"),
-//		message: m,
-//		choices:[
-//			{label: $L('Check In'), value:'ok', type:'affirmative'},
-//			{label: $L('Not Now'), value:'cancel', type:'color'}    
-//		]
-//	});	
-};
-NewAssistant.prototype.activate = function(event) {
-	toggleLoading("off");
-};
+			title: $L("Add Venue?"),
+			message: m,
+			choices:[
+				{label: $L('Add Venue'), value:'ok', type:'affirmative'},
+				{label: $L('Cancel'), value:'cancel', type:'color'}    
+			]
+		});	 	
+	},
+	addVenue: function(item){
+		banner("Adding " + item.titleNoFormatting + "...");
+		toggleLoading("on");
+		var hash = Base64.encode(user.login + ":" + user.password);
+		var params = "name=" + encodeURIComponent(item.titleNoFormatting) + "&address=" + encodeURIComponent(item.streetAddress) + "&city=" + encodeURIComponent(item.city) + "&state=" + encodeURIComponent(item.region) + "&geolat=" + encodeURIComponent(item.lat) + "&geolong=" + encodeURIComponent(item.lng);
+		var url = foursquare.apibase + 'addvenue.json';
+		var request = new Ajax.Request(url, {
+			method: 'POST',
+			evalJSON: 'force',
+			parameters: params,
+			requestHeaders: {
+					Authorization: "Basic " + hash
+				},
+			onSuccess: this.addedVenue.bind(this),
+			onFailure: function(transport){
+				toggleLoading("off");
+				ex(transport.responseJSON.error);
+			}
+		});	
+	},
+	addedVenue: function(transport){
+		toggleLoading("off");
+		banner("Venue " + transport.responseJSON.venue.name + " added!");
+		Mojo.Controller.stageController.popScene();
+	},
+	activate: function(event) {
+		toggleLoading("off");
+		 this.controller.listen("btnSearch", Mojo.Event.tap, this.btnSearchTapped.bind(this));
+		 this.controller.listen("results-list", Mojo.Event.listTap, this.listTapped.bind(this));
+	},
 
-NewAssistant.prototype.deactivate = function(event) {
-};
-
-NewAssistant.prototype.cleanup = function(event) {
+	deactivate: function(event) {
+		 this.controller.stopListening("btnSearch", Mojo.Event.tap, this.btnSearchTapped());
+		 this.controller.stopListening("results-list", Mojo.Event.listTap, this.listTapped());
+	}
 };
